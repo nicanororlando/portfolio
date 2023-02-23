edfd2dcbbffac69a9b95bc4317d453765c7ae5c9
nicanororlando <nicanororlando@gmail.com>
2023-02-23 11:00:30 -0300

    asd

--- src/routes/Default.tsx
+++ src/routes/Default.tsx
@@ -7,11 +7,7 @@ import Home from "pages/Home";
 const routes: React.FC = () => {
   return (
     <Switch>
-      <Route
-        path={`http://nicanororlando.github.io/portfolio`}
-        exact
-        component={Home}
-      />
+      <Route path={`${process.env.PUBLIC_URL}`} exact component={Home} />
     </Switch>
   );
 };
